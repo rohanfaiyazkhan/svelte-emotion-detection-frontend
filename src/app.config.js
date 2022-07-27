b9d0470e19c70e01915e80b78f99a4c4ae7edfbe
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2022-07-27 16:48:20 -0400

    Added production API url

--- src/app.config.js
+++ src/app.config.js
@@ -1,3 +1,6 @@
+const LOCAL_API_URL = 'http://localhost:7071/api/classify';
+const PRODUCTION_API_URL = 'https://emotiondetectionapp.azurewebsites.net/api/classify';
+
 export const Config = Object.freeze({
-	API_URL: process.env.NODE_ENV === 'development' ? 'http://localhost:7071/api/classify' : ''
+	API_URL: process.env.NODE_ENV === 'development' ? LOCAL_API_URL : PRODUCTION_API_URL
 });
