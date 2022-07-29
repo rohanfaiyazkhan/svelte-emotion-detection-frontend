43e9a5c6dc2c1b550e5269e90baff1eccc9e207a
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2022-07-29 14:34:51 -0400

    Manually commiting build files for Github pages. Will use actions for automated build later.

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,21 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs'
+		}),
 		prerender: {
 			default: true
+		},
+		paths: {
+			// change below to your repo name
+			base: isProd ? '/svelte-emotion-detection-frontend' : ''
 		}
 	},
 	preprocess: [
